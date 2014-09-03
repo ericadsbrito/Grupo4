2860020e1cf4702ddcf47d14642b78985f0bbefe
Patrick Borges <patrickborges@users.noreply.github.com>
2014-09-02 23:14:15 -0300

    Create Cod_02.cpp

---
+++ Cod_02.cpp
@@ -0,0 +1,12 @@
+//arrays as parameters
+
+#include <iostream>
+using namespace std;
+
+
+void printarray (int arg[], int length) 
+{
+	for (int n=0; n<length; n++)
+		cout << arg[n] << ' ';
+	cout << '\n';
+}
