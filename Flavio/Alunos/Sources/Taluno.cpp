0ebddba36f0e474a86321e03b46949aeac70f0a4
Flaviohmm <fhenrique609@gmail.com>
2014-10-17 12:46:27 -0300

    Create Taluno.cpp

---
+++ Flavio/Alunos/Sources/Taluno.cpp
@@ -0,0 +1,23 @@
+#include "metodoestatico.h"
+
+using namespace std;
+//---------------------------------------------------------Arquivo TAluno.cpp
+// Definição dos métodos
+void TPessoa::Entrada ()
+{
+
+cout << "Entre com o nome do aluno: ";
+getline (cin, nome);
+cout << "Entre com a matricula do aluno: ";
+getline (cin, matricula);
+cout << "Entre com o IAA do aluno: ";
+cin >> iaa;
+cin.get ();
+
+}
+void TPessoa::Saida () const
+{
+cout << "Nome do aluno: " << nome << endl;
+cout << "Matricula : " << matricula << endl;
+cout << "iaa : " << iaa << endl;
+}
